3ac5287fc284e8abd0dde0daf7076d66547b2f56
TokiDOkiiiiiiii <napatphong46@gmail.com>
2024-05-10 16:02:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+/*
+height
+autoButton
+autoStatus
+pumpButton
+pumpStatus
+*/
+
+document.getElementById("height").innerHTML = 9;
+
+document.getElementById("autoButton").addEventListener("click", function(){
+    var status = document.getElementById("autoStatus").innerHTML
+    if (status === "On"){
+        document.getElementById("autoStatus").innerHTML = "Off"
+    }
+    else {
+        document.getElementById("autoStatus").innerHTML = "On"
+    }
+});
+document.getElementById("pumpButton").addEventListener("click", function(){
+    var status = document.getElementById("pumpStatus").innerHTML
+    if (status === "On"){
+        document.getElementById("pumpStatus").innerHTML = "Off"
+    }
+    else {
+        document.getElementById("pumpStatus").innerHTML = "On"
+    }
+});
+
+function initWebsite(){
+    //Get JSON
+    //document.getElementById("height").innerHTML = !
+    //document.getElementById("autoStatus").innerHTML = !
+    //document.getElementById("pumpStatus").innerHTML = !
+    
+}
+//SetUP the current status

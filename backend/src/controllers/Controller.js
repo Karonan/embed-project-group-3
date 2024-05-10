9d74b923095ffbd83e6461a23bb9ef1fb7766fab
Pongpak Manoret <pongpak282546@gmail.com>
2024-05-10 16:24:52 +0700

    Add all api

--- backend/src/controllers/Controller.js
+++ backend/src/controllers/Controller.js
@@ -1,10 +1,26 @@
+var Data = {pumpMode:-1,waterHeight:0,pumpStatus:-1};
+console.log(Data);
+export const updateHeight = async (req, res) => {
+  Data.waterHeight = req.body.height
+  res.status(200).json({ message: "OK" });
+};
 
-export const createItem = async (req, res) => {
-  console.log(req.body);
+export const togglePump = async (req, res) => {
+  pumpMode = pumpMode*(-1);
   res.status(200).json({ message: "OK" });
 };
 
 
-export const getItems = async (req, res) => {
-  res.status(200).json(items);
+export const setMode = async (req, res) => {
+  pumpMode = pumpStatus*(-1);
+  res.status(200).json({ message: "OK" });
 };
+
+export const getStatus = async (req, res) => {
+  res.status(200).json(Data);
+};
+
+
+/*
+res.status(200).json(items);
+*/
--- backend/src/routes/itemRoute.js
+++ backend/src/routes/itemRoute.js
@@ -4,7 +4,9 @@ import * as Controller from "../controllers/Controller.js";
 
 const router = express.Router();
 
-router.get("/", Controller.getItems);
-router.post("/", Controller.createItem);
+router.get("/status", Controller.getStatus);
+router.post("/postHeight", Controller.updateHeight);
+router.post("/toggle", Controller.togglePump);
+router.post("/setMode", Controller.setMode);
 
 export default router;

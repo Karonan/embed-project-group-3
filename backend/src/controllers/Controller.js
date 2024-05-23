a5a566c13b5ccece011f4d721d84bc312780111a
Pongpak Manoret <peaw@instance-20240521-093421.c.burnished-case-424009-b2.internal>
2024-05-23 09:46:14 +0000

    update set threshold

--- backend/src/controllers/Controller.js
+++ backend/src/controllers/Controller.js
@@ -1,5 +1,6 @@
-var Data = {pumpMode:-1,waterHeight:0,pumpStatus:-1};
-console.log(Data);
+var Data = {pumpMode:-1,waterHeight:0,pumpStatus:-1,waterThreshold:0};
+
+
 export const updateHeight = async (req, res) => {
   Data.waterHeight = req.body.height
   res.status(200).json({ message: "OK" });
@@ -8,18 +9,21 @@ export const updateHeight = async (req, res) => {
 export const togglePump = async (req, res) => {
   Data.pumpStatus = Data.pumpStatus*(-1);
   res.status(200).json({ message: "OK" });
-  console.log(Data);
 };
 
 
 export const setMode = async (req, res) => {
   Data.pumpMode = Data.pumpMode*(-1);
   res.status(200).json({ message: "OK" });
-  console.log(Data);
 };
 
 export const getStatus = async (req, res) => {
   res.status(200).json(Data);
+};
+
+export const setWaterThreshold = async (req, res) => {
+  Data.waterThreshold = req.body.level
+  res.status(200).json({ message: "OK" });
 };
 
 
